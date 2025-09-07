98870f0472ef6700abda7c07e2cf5b95f5aa6585
anamolhasan <mdanamolhasan1998@gmail.com>
2025-09-07 20:18:05 +0600

    dynamic route

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -14,6 +14,7 @@ const Navbar = () => {
           <Link href={'/'}> <li>Home</li> </Link>
           <Link href={'/services'}> <li>Services</li> </Link>
           <Link href={'/about'}> <li>About</li> </Link>
+          <Link href={'/blogs'}> <li>Blogs</li> </Link>
         </ul>
     </div>
   )
